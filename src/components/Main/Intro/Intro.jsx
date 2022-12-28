d7c6bf4a30380554d499919b026da4936c904330
Vladimir Ivancha <Vladimir.Ivancha@mail.ru>
2022-12-29 00:34:30 +0300

    feat: add intro

--- src/components/Main/Intro/Intro.jsx
+++ src/components/Main/Intro/Intro.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 function Intro() {
   return (
     <section className="intro">
-      
+      <div className='intro__clouds'></div>
     </section>
   );
 }
--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -1,9 +1,10 @@
 import React from 'react';
+import Intro from './Intro/Intro';
 
 function Main() {
   return (
     <main className="main">
-      
+      <Intro />
     </main>
   );
 }
