d563d2999d6d1fabaae2a13e9cebec9d5ab18c92
Vladimir Ivancha <Vladimir.Ivancha@mail.ru>
2022-12-29 00:13:58 +0300

    feat: start main

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import Header from './components/Header/Header';
+import Main from './components/Main/Main';
 import './pages/index.css';
 
 function App() {
   return (
     <div className="app">
       <Header />
+      <Main />
     </div>
   );
 }
---
+++ src/components/Main/Intro/Intro.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+function Intro() {
+  return (
+    <section className="intro">
+      
+    </section>
+  );
+}
+export default Intro;
