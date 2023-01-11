888bc88fbca9b0dac33b20846e07c538fe048aaf
Vladimir Ivancha <Vladimir.Ivancha@mail.ru>
2023-01-12 00:20:27 +0300

    feat: add newsletter js code

--- src/components/Main/Newsletter/Newsletter.jsx
+++ src/components/Main/Newsletter/Newsletter.jsx
@@ -8,9 +8,13 @@ import ActionButton from "../../Buttons/Action/ActionButton";
 function Newsletter() {
   const form = useFormValidation();
   const [disabled, setDisabled] = useState(true);
+  const [placeholder, setPlaceholder] = useState('Email address')
+  
   function handleSubmit(e) {
     e.preventDefault();
     setDisabled(true);
+    form.resetForm();
+    setPlaceholder('E-mail succesfully saved')
   }
   useEffect(() => {
     setDisabled(!form.isValid);
@@ -20,24 +24,27 @@ function Newsletter() {
     <section className="newsletter">
       <HpTextArea data={newsletterHpTextArea} />
       <form
-        className="newsletter__form"
+        className="form newsletter__form"
         id="sign_up"
         name="sign_up"
         onSubmit={handleSubmit}
         noValidate
       >
         <TextInput
-          className="newsletter__input"
+          labelClassName="newsletter__label"
+          inputClassName="newsletter__input"
+          errorClassName="newsletter__input-error newsletter__input-error_active"
           name="email"
           type="email"
+          placeholder={placeholder}
           pattern="^[\w]+@[a-zA-Z]+\.[a-zA-Z]{1,3}$"
           value={form.values.email || ""}
           onChange={form.handleChange}
           errorMessage={form.errors.email}
         />
         <ActionButton 
-            name={"Save me"} 
-            className={"newsletter__btn"} 
+            name={"Save me"}
+            className={`newsletter__btn ${disabled && 'newsletter__btn_disabled'}`} 
             type="submit" 
             onClick={handleSubmit}
             disabled={disabled}
--- src/utils/TextInput.jsx
+++ src/utils/TextInput.jsx
@@ -1,22 +1,32 @@
 import React from 'react';
 
 function TextInput({
-  name, type, value, onChange, errorMessage, pattern, className,
+  name, 
+  type, 
+  value, 
+  onChange, 
+  errorMessage, 
+  pattern,
+  inputClassName, 
+  placeholder, 
+  labelClassName, 
+  errorClassName,
 }) {
 
   return (
-    <label className="loginregform__label" htmlFor={name}>
+    <label className={labelClassName} htmlFor={name}>
       <input
-        className={className} // loginregform__input
+        className={inputClassName}
         name={name}
         id={name}
+        placeholder={placeholder}
         type={type}
         value={value}
         onChange={onChange}
         pattern={pattern}
         required
       />
-      <span className="form__input-error form__input-error_active">{errorMessage}</span>
+      <span className={errorClassName}>{errorMessage}</span>
     </label>
   );
 }
