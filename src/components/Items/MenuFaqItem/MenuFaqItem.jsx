cdecdc1e0f38e24868f31153dbc6d3306f490ed7
Vladimir Ivancha <Vladimir.Ivancha@mail.ru>
2023-01-12 22:46:20 +0300

    feat: add faq js code

--- src/components/Items/MenuFaqItem/MenuFaqItem.jsx
+++ src/components/Items/MenuFaqItem/MenuFaqItem.jsx
@@ -1,14 +1,28 @@
-import React from "react";
+import React, { useState } from "react";
 import ActionButton from "../../Buttons/Action/ActionButton";
 
 function MenuFaqItem({ item }) {
+    const [arrowActive, setArrowActive] = useState(false);
+    const [questionActive, setQuestionActive] = useState(false);
+    const [bodyActive, setBodyActive] = useState(false);
+
+    const arrowClass = arrowActive ? item.faqArrowClassNameActive : item.faqArrowClassName;
+    const questionClass = questionActive ? item.questionClassNameActive : item.questionClassName;
+    const bodyClass = bodyActive ? item.faqBodyClassName : item.faqBodyClassNameActive;
+
+    function handleQuestionClick() {
+        setArrowActive(!arrowActive);
+        setQuestionActive(!questionActive);
+        setBodyActive(!bodyActive);
+    }
+
   return (
     <li className={item.mainClassName} id={item.id}>
-      <div className={item.faqHeaderClassName}>
-        <div className={item.faqArrowClassName}></div>
-        <h3 className={item.questionClassName}>{item.questionText}</h3>
+      <div className={item.faqHeaderClassName} onClick={handleQuestionClick}>
+        <div className={arrowClass}></div>
+        <h3 className={questionClass}>{item.questionText}</h3>
       </div>
-      <div className={item.faqBodyClassName}>
+      <div className={bodyClass}>
         <p className={item.answerClassName}>{item.answerText}</p>
         <ActionButton
           name={item.btnText}
